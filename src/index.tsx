daa23fba5f08dc93cde83eb9d93300af2a1b0a3f
David Goodman <davidgoodman@davids-air.myfiosgateway.com>
2023-09-22 11:56:02 -0400

    import esbuild wasm

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,34 @@
-import ReactDOM from 'react-dom';
+import * as esbuild from 'esbuild-wasm';
+import { createRoot } from 'react-dom/client';
+import { useState } from 'react';
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
 const App = () => {
-  return <h1>App</h1>
-}
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+root.render(<App />);
