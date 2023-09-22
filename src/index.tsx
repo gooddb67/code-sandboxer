71484bcd54e3d1c4f2657d78c1f8ae902f23b7c3
David Goodman <davidgoodman@davids-air.myfiosgateway.com>
2023-09-22 12:19:29 -0400

    transpile via wasm

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,37 @@
 import * as esbuild from 'esbuild-wasm';
 import { createRoot } from 'react-dom/client';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm',
     });
   };
 
-  const onClick = () => {
-    console.log(input);
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
   };
 
   return (
